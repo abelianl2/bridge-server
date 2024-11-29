aaeb9f2f9b2171c377e63ae6150279c41dc1ec55
sunjiangjun <nihaojiangjun@163.com>
2024-11-29 18:17:56 +0800

    build: update

--- docker/docker-entrypoint-initdb.d/bridge.sql
+++ docker/docker-entrypoint-initdb.d/bridge.sql
@@ -1,24 +1,9 @@
-/*
- Navicat MySQL Data Transfer
-
- Source Server         : localhost
- Source Server Type    : MySQL
- Source Server Version : 80036
- Source Host           : localhost:3306
- Source Schema         : bridge
-
- Target Server Type    : MySQL
- Target Server Version : 80036
- File Encoding         : 65001
-
- Date: 28/04/2024 22:38:17
-*/
-
+CREATE
+DATABASE IF NOT EXISTS `bridge`;
+USE bridge;
 SET NAMES utf8mb4;
 SET
 FOREIGN_KEY_CHECKS = 0;
-CREATE
-DATABASE IF NOT EXISTS `bridge`;
 -- ----------------------------
 -- Table structure for bridge
 -- ----------------------------
--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/abelianl2/bridge-server
 
-go 1.21.4
+go 1.23.2
 
 require (
 	github.com/gin-contrib/cors v1.7.2
